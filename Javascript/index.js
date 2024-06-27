c51b089a8028c646a85ea29550c5976a569d4a3c
MinkeBerg <167294739+MinkeBerg@users.noreply.github.com>
2024-06-27 12:10:46 +0200

    Added JS real time and date cities homepage

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,1 +1,29 @@
+function updateTime() {
+  let newYorkElement = document.querySelector("#new-york");
+  let newYorkDateElement = newYorkElement.querySelector(".date");
+  let newYorkTimeElement = newYorkElement.querySelector(".time");
+  let newYorkTime = moment().tz("America/New_York");
+  newYorkDateElement.innerHTML = moment().format("MMM Do YYYY");
+  newYorkTimeElement.innerHTML = newYorkTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
 
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = londonElement.querySelector(".date");
+  let londonTimeElement = londonElement.querySelector(".time");
+  let londonTime = moment().tz("Europe/London");
+  londonDateElement.innerHTML = moment().format("MMM Do YYYY");
+  londonTimeElement.innerHTML = londonTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  let tokioElement = document.querySelector("#tokio");
+  let tokioDateElement = tokioElement.querySelector(".date");
+  let tokioTimeElement = tokioElement.querySelector(".time");
+  let tokioTime = moment().tz("Asia/Tokyo");
+  tokioDateElement.innerHTML = moment().format("MMM Do YYYY");
+  tokioTimeElement.innerHTML = tokioTime.format("h:mm:ss [<small>]A[</small>]");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
